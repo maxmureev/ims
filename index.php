6a1ce0b44d3895c3ca538f73b0b1e6371e2c133f
Max Mureev <m.mureev@qoden.com>
2022-11-04 00:11:50 +0700

    commit this

---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+<?php
+ $p = 'Это переменная из условия задачи';
+?>
+
+<?php
+ $name = 'Max';
+ $surname = 'Mureev';
+ $city = 'Гражданином мира';
+ $age = '~39';
+?>
+
+
+<?php
+include 'main.php';
+?>
